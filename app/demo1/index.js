322e474b609c942cfa5b7131c49cf5a8a3ad84ec
chenpeng <cp1001@126.com>
2018-05-11 19:36:47 +0800

    demo1

---
+++ app/demo1/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+const http2 = require('spdy');
+// const http2 = require('http2');
+const path = require('path');
+
+const options = {
+    key: fs.readFileSync('./app/demo1/keys/server-key.pem'),
+    cert: fs.readFileSync('./app/demo1/keys/server-cert.pem')
+};
+
+const app = new express();
+http2.createServer(options, app).listen(8080, () => {
+    console.log(`Server is listening on https://localhost:8080. You can open the URL in the browser.`);
+});
+//app.use('/', express.static(path.join(__dirname, './assets')));
+
+app.get("/", (req, res) => {
+    var stream = res.push('/app.js', {   //服务器推送
+        status: 200, // optional
+        method: 'GET', // optional
+        request: {
+            accept: '*/*'
+        },
+        response: {
+            'content-type': 'application/javascript'
+        }
+    })
+    stream.on('error', function () {
+    })
+    stream.end('console.log("http2 push stream, by Lucien ");')
+
+    res.send(`hello http2!
+    <script src="/app.js"></script>`);//express 并没有host static ,这个app.js 来自push 
+})
---
+++ readme.md
@@ -0,0 +1,10 @@
+### 安装
+```
+yarn
+```
+
+### demo1 简单http2
+```
+npm run demo1
+```
+
